0f2c21b1e73e50090ce1e540051aa3676878cac9
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-02-08 13:18:57 +0200

    reinstalat npm dependinte

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import {defineConfig} from 'vite';
+import hydrogen from '@shopify/hydrogen/plugin';
+import shopifyConfig from './shopify.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [hydrogen(shopifyConfig)],
+});
